f55d385b84e0b8998507ee049a06e085b2212d13
ban367 <ban367@gosoly.com>
2024-05-04 20:19:09 +0900

    :+1: add save add user

--- src/lib/components/UserList.svelte
+++ src/lib/components/UserList.svelte
@@ -1,10 +1,17 @@
 <script lang="ts">
+  import { createEventDispatcher } from "svelte";
   import { Autocomplete } from "@skeletonlabs/skeleton";
   import type { AutocompleteOption } from "@skeletonlabs/skeleton";
   import type { UserData } from "$lib/types/user";
+  import type { InputEventDetail } from "$lib/types/form";
 
   export let userList: UserData[];
 
+  interface Events {
+    input: InputEventDetail;
+  }
+
+  const dispatch = createEventDispatcher<Events>();
   const convertedUsers: AutocompleteOption<string>[] = userList.map((user) => ({
     value: user.id.toString(),
     label: user.name,
@@ -15,10 +22,24 @@
   function onFlavorSelection(event: CustomEvent<AutocompleteOption<string>>): void {
     inputName = event.detail.label;
   }
+
+  function handleInput(event: Event) {
+    const inputElement = event.target as HTMLInputElement;
+    inputName = inputElement.value;
+  }
+
+  $: dispatch("input", { value: inputName });
 </script>
 
 <div>
-  <input class="input" type="search" name="demo" bind:value={inputName} placeholder="Search..." />
+  <input
+    class="input"
+    type="search"
+    name="demo"
+    bind:value={inputName}
+    on:input={handleInput}
+    placeholder="Search..."
+  />
   <div class="card max-h-48 w-full max-w-sm overflow-y-auto p-4" tabindex="-1">
     <Autocomplete bind:input={inputName} options={convertedUsers} on:selection={onFlavorSelection} />
   </div>
--- src/lib/components/modal/Submit.svelte
+++ src/lib/components/modal/Submit.svelte
@@ -1,36 +1,17 @@
 <script lang="ts">
   import { getModalStore } from "@skeletonlabs/skeleton";
   import type { SvelteComponent } from "svelte";
-  import type { UserData } from "$lib/types/user";
-  import type { InputEventDetail } from "$lib/types/form";
-
-  import UserList from "$lib/components/UserList.svelte";
 
   export let parent: SvelteComponent;
   export let title: string;
-  export let userList: UserData[];
 
   const modalStore = getModalStore();
 
-  let selectUserId = 0;
-
   function onConfirm(): void {
     if (typeof $modalStore[0]?.response === "function") {
-      $modalStore[0].response(selectUserId);
+      $modalStore[0].response(true);
     }
     modalStore.close();
-  }
-
-  function findUserIdByName(users: UserData[], searchName: string): number | undefined {
-    const user = users.find((user) => user.name === searchName);
-    return user?.id;
-  }
-
-  function handleInput(event: CustomEvent<InputEventDetail>) {
-    const userId = findUserIdByName(userList, event.detail.value);
-    if (userId) {
-      selectUserId = userId;
-    }
   }
 
   const cButton = "btn bg-white border border-gray-400 px-12 focus:!outline-none";
@@ -40,7 +21,7 @@
   <div class="w-modal relative rounded-lg bg-white p-8 shadow-xl">
     <p class="text-xl font-bold">{title}</p>
     <div class="mb-8 mt-2">
-      <UserList {userList} on:input={handleInput} />
+      <slot />
     </div>
     <div class="space-x-2 text-right">
       <button type="button" class={cButton} on:click={onConfirm}>はい</button>
