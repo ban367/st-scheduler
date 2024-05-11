af02fdb2d7939b88db97ee7d1c34ab11d7716b6d
ban367 <ban367@gosoly.com>
2024-05-11 23:11:35 +0900

    :+1: add maintenance component

---
+++ src/lib/components/Maintenance.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+  import Icon from "@iconify/svelte";
+  export let message = "調整中";
+</script>
+
+<div class="relative flex min-h-screen items-center justify-center">
+  <div class="absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 transform text-center">
+    <div class="flex items-center">
+      <Icon icon="mdi:scheduled-maintenance" height="30" />
+      <p class="ml-2 text-2xl">{message}</p>
+    </div>
+  </div>
+</div>
