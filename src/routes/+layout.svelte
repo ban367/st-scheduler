c0c41c3a51502263f6e39e3b2075b2896fa60d6c
ban367 <ban367@gosoly.com>
2024-05-19 16:15:12 +0900

    :+1: fix start highlite

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,11 +1,23 @@
 <script lang="ts">
   import "../app.postcss";
+  import { onMount } from "svelte";
+  import { writable } from "svelte/store";
   import { page } from "$app/stores";
   import { AppRail, AppRailAnchor } from "@skeletonlabs/skeleton";
   import { Toast, Modal, initializeStores } from "@skeletonlabs/skeleton";
   import Icon from "@iconify/svelte";
 
   initializeStores();
+
+  const currentPath = writable("");
+
+  onMount(() => {
+    currentPath.set(window.location.pathname);
+  });
+
+  page.subscribe(($page) => {
+    currentPath.set($page.url.pathname);
+  });
 </script>
 
 <Modal />
@@ -25,22 +37,22 @@
           </div>
         </AppRailAnchor>
       </svelte:fragment>
-      <AppRailAnchor href="/" selected={$page.url.pathname === "/"}>
+      <AppRailAnchor href="/" selected={$currentPath === "/"}>
         <div class="flex h-full items-center justify-center">
           <Icon icon="mdi:home" height="26" />
         </div>
       </AppRailAnchor>
-      <AppRailAnchor href="/user" selected={$page.url.pathname === "/user"}>
+      <AppRailAnchor href="/user" selected={$currentPath === "/user"}>
         <div class="flex h-full items-center justify-center">
           <Icon icon="mdi:user" height="26" />
         </div>
       </AppRailAnchor>
-      <AppRailAnchor href="/calendar" selected={$page.url.pathname === "/calendar"}>
+      <AppRailAnchor href="/calendar" selected={$currentPath === "/calendar"}>
         <div class="flex h-full items-center justify-center">
           <Icon icon="mdi:calendar" height="26" />
         </div>
       </AppRailAnchor>
-      <AppRailAnchor href="/settings" selected={$page.url.pathname === "/settings"}>
+      <AppRailAnchor href="/settings" selected={$currentPath === "/settings"}>
         <div class="flex h-full items-center justify-center">
           <Icon icon="mdi:settings" height="26" />
         </div>
